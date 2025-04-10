87320a9265fcbfe7395ebabac4eda969f1f0dff9
rafaelsatyro <rafael.satyro@hotmail.com>
2025-04-10 04:08:25 -0300

    feat: auto-worker-inserted

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import './workers/worker';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   // Swagger config
   const config = new DocumentBuilder()
     .setTitle('Monzy API')
