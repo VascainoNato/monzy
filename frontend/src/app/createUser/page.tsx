cd0735c29c856d67d470cdf7d05ac6d6a7b7185e
rafaelsatyro <rafael.satyro@hotmail.com>
2025-04-11 03:57:34 -0300

    feat: inserted-new-create-user-page

--- frontend/src/app/createUser/page.tsx
+++ frontend/src/app/createUser/page.tsx
@@ -3,13 +3,10 @@ import { useRouter } from 'next/navigation';
 import Image from "next/image";
 import back from "../../../public/icon-back.png";
 import monzy from "../../../public/monzy.png";
-import useAuth from '../hooks/useAuth';
-import google from "../../../public/google.png";
-import monzyWelcome from "../../../public/monzy-welcome-2.png";
+import monzyWelcome from "../../../public/monzy-create-user.png";
 
 export default function Login() {
   const router = useRouter();
-  const  {handleGoogleLogin}  = useAuth();
   return (
     <>
         <div className="flex w-full flex-col md:flex-row ">
@@ -21,10 +18,10 @@ export default function Login() {
                         width={50}
                         height={60}
                         className="cursor-pointer"
-                        onClick={() => router.push('/')}
+                        onClick={() => router.push('/login')}
                     />
                     <h1 className="flex font-normal text-xl text-indigo-500">|</h1>
-                    <h1 className="flex font-normal text-xl text-indigo-500 pt-1 cursor-pointer">Login</h1>
+                    <h1 className="flex font-normal text-xl text-indigo-500 pt-1 cursor-pointer">Create User</h1>
                 </div>
                 <div className="flex flex-col pt-4 sm:pt-12.5 md:pt-12.5 lg:pt-6 2xl:pt-5 items-center">
                     <Image
@@ -42,22 +39,11 @@ export default function Login() {
                     <div className="flex flex-col pt-12">
                         <p className="flex w-full font-normal text-teal-500 pb-1">Password</p>
                         <input type="text" placeholder="Insert your email here..." className='border rounded border-indigo-500 pt-2 pr-2 pb-2 pl-4 outline-indigo-500 text-indigo-500 placeholder-gray-400' />
-                        <p className='flex w-full justify-end pt-2 font-normal text-teal-500 cursor-pointer'>Forgot password</p>
                     </div>
                 </div>
                 <div className='flex w-full flex-col pt-20 sm:pt-16 md:pt-14 lg:pt-12 2xl:pt-20'>
                     <div className='flex justify-center gap-2'>
-                        <button className='bg-teal-500 p-2 rounded text-white w-40 cursor-pointer'>Enter</button>
-                        <button onClick={handleGoogleLogin} className='bg-indigo-500 rounded cursor-pointer'>
-                            <Image
-                                src={google}
-                                alt="google icon"
-                                className='w-10 h-10 p-2 flex items-center'
-                            />
-                        </button>
-                    </div>
-                    <div className='flex justify-center items-center pt-20 sm:pt-18 md:pt-14 lg:pt-12 2xl:pt-20'>
-                        <p className='font-normal text-teal-500 underline underline-offset-8 cursor-pointer' onClick={() => router.push('/createUser')}>Create User</p>
+                        <button className='bg-teal-500 p-2 rounded text-white w-45 cursor-pointer'>Create User</button>
                     </div>
                 </div>
           </div>
